b0e7dfad835ece44a63606106da8e96b3c60e067
lihe07 <li@imlihe.com>
2023-08-23 15:40:10 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
 import solid from "solid-start/vite";
+import netlify from "solid-start-netlify";
 import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: "127.0.0.1",
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  plugins: [
-    solid({
-      adapter: "solid-start-netlify",
-    }),
-    unocss(),
-  ],
+	server: {
+		port: 3000,
+		host: "127.0.0.1",
+	},
+	resolve: {
+		alias: {
+			"@": "/src",
+		},
+	},
+	plugins: [
+		solid({
+			adapter: netlify({ edge: true }),
+		}),
+		unocss(),
+	],
 });
