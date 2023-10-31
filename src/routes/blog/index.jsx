04db3f6b586d1b789fde3de4325eb0a1ed0b05d7
lihe07 <li@imlihe.com>
2023-10-31 17:54:41 +0800

    fix

--- src/routes/blog/index.jsx
+++ src/routes/blog/index.jsx
@@ -1,5 +1,5 @@
-import { createResource, For } from "solid-js";
-import { A, Title, useRouteData } from "solid-start";
+import { For } from "solid-js";
+import { Title, useNavigate } from "solid-start";
 import Section from "@/components/Section";
 import PageHead from "@/components/PageHead";
 import blog from "@/blog";
@@ -7,24 +7,26 @@ import { BlogDescription } from "@/components/blog/BlogDescription";
 import Tag from "@/components/blog/Tag";
 
 function Blog(props) {
+	const navigate = useNavigate();
 	return (
-		<A
-			class="flex decoration-none color-white font-sans gap-10 bg-zinc-9 rounded-2xl transition-all-150 op-80 hover:op-100 active:scale-95"
-			href={props.href}
+		<div
+			class="flex cursor-pointer decoration-none color-white font-sans gap-10 bg-zinc-9 rounded-2xl transition-all-150 op-80 hover:op-100 active:scale-95"
+			onClick={() => navigate(props.href)}
+			onKeyDown={() => navigate(props.href)}
 		>
 			<img src={props.cover} alt="" class="rounded-2xl h-60 w-40%" />
-			<div class="py-3">
-				<h2 class="text-4xl">{props.title}</h2>
+			<div class="py-3 flex flex-col justify-center">
+				<h2 class="text-4xl mt-0">{props.title}</h2>
 
 				<div class="flex">
-					<For each={props.tags}>{(e) => <Tag {...e} />}</For>
+					<For each={props.tags}>{(e) => <Tag hover={false} {...e} />}</For>
 				</div>
 
-				<p class="text-xl op-70">
+				<p class="text-xl op-70 mb-0">
 					<BlogDescription {...props} />
 				</p>
 			</div>
-		</A>
+		</div>
 	);
 }
 
@@ -39,7 +41,7 @@ export default () => {
 			/>
 
 			<Section class="my-20">
-				<div class="grid md:grid-cols-2 grid-cols-1">
+				<div class="grid md:grid-cols-2 grid-cols-1" id="blogs">
 					<For each={blog}>{(e) => <Blog {...e} key={e.slug} />}</For>
 				</div>
 			</Section>
--- src/tags.js
+++ src/tags.js
@@ -2,13 +2,16 @@ export default {
 	technologies: {
 		name: "Technologies",
 		color: "#0ea5e9",
+		id: "technologies",
 	},
 	"quantum-chemistry": {
 		name: "Quantum Chemistry",
 		color: "#134e4a",
+		id: "quantum-chemistry",
 	},
 	or: {
 		name: "Opearations Research",
 		color: "#6366f1",
+		id: "or",
 	},
 };
